b53d14194bc9be9d4ee954c4186b9fae5d687624
Kishan Singh <your-email@domain.com>
2025-10-02 20:45:44 +0530

    ”a”

--- src/components/home/video.jsx
+++ src/components/home/video.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const video = () => {
   return (
-    <div>
-      
+    <div className='h-full w-full'>
+      <video className='h-full w-full object-cover' autoPlay loop muted src='https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1759417368~exp=1759420968~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=ee545612a4c23733530a8a4d7e075b1f69c3b27798bb0731f391bd19ba083b51&r=dXMtZWFzdDE%3D'></video>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Video from "../components/home/video";
+import HomeHeroText from '../components/home/HomeBottomText'
+import HomeText from '../components/home/HomeBottomText'
 const HomeheroText = () => {
   return (
-    <div className='h-screen w-screen bg-red-900 '>
-      
+    <div className="h-screen w-screen fixed ">
+     
+
+      <div className="h-screen w-screen relative">
+        <HomeHeroText />
+        <HomeBottomText />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeheroText
-
+export default HomeheroText;
